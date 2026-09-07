b8db2d0484fbcbb98b43d797cf1e2f187e11263e
Nisura Indisa <64645496+nisuraaa@users.noreply.github.com>
2026-09-07 18:07:40 +0530

    Add URL Validation Pattern

--- src/main/java/com/indezah/url_shortener/dto/CreateUrlRequest.java
+++ src/main/java/com/indezah/url_shortener/dto/CreateUrlRequest.java
@@ -1,6 +1,7 @@
 package com.indezah.url_shortener.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.URL;
@@ -10,5 +11,6 @@ import org.hibernate.validator.constraints.URL;
 public class CreateUrlRequest {
     @NotBlank(message = "URL must not be empty")
     @URL(message = "originalUrl must be a valid URL")
+    @Pattern(regexp = "^https?://.*", message = "URL must start with http:// or https://")
     private String originalUrl;
 }
--- src/main/java/com/indezah/url_shortener/dto/UpdateUrlRequest.java
+++ src/main/java/com/indezah/url_shortener/dto/UpdateUrlRequest.java
@@ -1,6 +1,7 @@
 package com.indezah.url_shortener.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.URL;
@@ -12,5 +13,6 @@ public class UpdateUrlRequest {
     private String shortUrl;
     @NotBlank(message = "URL must not be empty")
     @URL(message = "originalUrl must be a valid URL")
+    @Pattern(regexp = "^https?://.*", message = "URL must start with http:// or https://")
     private String newOriginalUrl;
 }
